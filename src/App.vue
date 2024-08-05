670ae62821f8df27bdf4f961e2b068f7b1789615
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-08-05 06:26:19 +0300

    Заменил @media в .container на clamp()

--- src/App.vue
+++ src/App.vue
@@ -14,17 +14,8 @@
 
 .container {
   max-width: pxToRem(1250);
+  padding-inline: clamp(1.25rem, 5vw, 3.75rem);
   margin: 0 auto;
-
-  @media (min-width: calc($tablet + 1px)) {
-    padding-inline: 60px;
-  }
-  @media (max-width: $tablet) and (min-width: calc($mobile + 1px)) {
-    padding-inline: 40px;
-  }
-  @media (max-width: $mobile) {
-    padding-inline: 20px;
-  }
 }
 </style>
 
